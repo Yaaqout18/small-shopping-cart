78c65716f00705c8818b35fe44c9b65693b23195
yaaqout <27elruby@gmail.com>
2024-05-04 22:00:41 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let headerElement = document.getElementById('headerElement');
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 100){
+headerElement.classList.add("scroll-down")
+  }else{
+    headerElement.classList.remove("scroll-down")
+  }
+}
+)
